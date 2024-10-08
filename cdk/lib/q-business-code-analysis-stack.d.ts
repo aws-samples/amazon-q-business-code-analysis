42470cacb7ecd56ab0ae4398dd581f9426b5143c
Jim Higgins <jwhigg@amazon.com>
2024-10-08 13:31:55 -0500

    Remove CDK generated files.

--- cdk/lib/q-business-code-analysis-stack.d.ts
+++
@@ -1,5 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-export declare class QBusinessCodeAnalysisStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps);
-}
